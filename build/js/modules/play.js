1e69ba0b937b6b082208b091012a21b28d96ce9e
Anastasia Konopatova <32372877+konopatova@users.noreply.github.com>
2019-12-26 18:05:58 +0300

    правка videos

--- build/js/modules/play.js
+++ build/js/modules/play.js
@@ -28,6 +28,8 @@
   var btn = $('#top-button');
 
   $(window).scroll(function () {
+    btn.removeClass('show');
+
     if ($(window).scrollTop() > 300) {
       btn.addClass('show');
     } else {
